cd2f99b432148595776f471f293b8f078430fb81
Kate <kit.ty.kate@disroot.org>
2018-07-08 09:56:20 +0100

    Explain how to get the gif vs. video feature in the README instead of displaying a warning

--- src/gif_detect_dummy.ml
+++ src/gif_detect_dummy.ml
@@ -1,4 +1,1 @@
-let () = prerr_endline "Warning: Couldn't distinguish gifs from videos. Please recompile with the ffmpeg library \
-                        installed if you want this feature"
-
 let is_gif _ = false
