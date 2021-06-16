5c6f2ce0db1a1ab06d13120a006a8f13d1ab6dfd
Kate <kit.ty.kate@disroot.org>
2021-06-16 17:28:06 +0100

    Upgrade to ocaml-ffmpeg 1.0

--- src/gif_detect.ml
+++ src/gif_detect.ml
@@ -1,7 +1,7 @@
 type filename = string
 
 let is_gif url =
-  let input = FFmpeg.Av.open_input url in
-  let audio = FFmpeg.Av.get_audio_streams input in
-  FFmpeg.Av.close input;
+  let input = Av.open_input url in
+  let audio = Av.get_audio_streams input in
+  Av.close input;
   List.is_empty audio
